4772a7da744aa14c26802d08065ad9fd51cae1ec
Jahangir Ahmad <jahangir@studio137.co.za>
2023-08-21 11:26:02 +0530

    Updates

--- index.js
+++ index.js
@@ -1,76 +1,47 @@
 const fs = require('fs');
 const path = require('path');
 const { execSync } = require('child_process');
+const prettier = require('prettier');
 
 function generateProjectStructure(projectName) {
   const baseDir = path.join(process.cwd(), projectName);
 
-  const dirs = [
-    'controllers/v1',
-    'controllers/v2',
-    'controllers/v3',
-    'routes/v1',
-    'routes/v2',
-    'routes/v3',
-    'src/Model',
-    'src/middleware',
-    'config'
-  ];
+  // Create the main project directory
+  fs.mkdirSync(baseDir);
 
-  dirs.forEach((dir) => {
-    fs.mkdirSync(path.join(baseDir, dir), { recursive: true });
-  });
-
-  // Generate db_connection.js
-  const dbConnectionPath = path.join(baseDir, 'config/db_connection.js');
-  const dbConnectionCode = `
-    // db_connection.js content
-    // You can place your database connection code here
+  // Create config directory and db_credientials.js
+  const configDir = path.join(baseDir, 'config');
+  fs.mkdirSync(configDir);
+  const dbCredentialsCode = `
+    // db_credentials.js content
+    // You can place your database credentials here
     `;
-  fs.writeFileSync(dbConnectionPath, dbConnectionCode.trim());
+  fs.writeFileSync(path.join(configDir, 'db_credentials.js'), dbCredentialsCode.trim());
 
-  // Generate package.json
-  const packageJsonPath = path.join(baseDir, 'package.json');
-  const packageJsonContent = JSON.stringify({
-    name: projectName,
-    version: '1.0.0',
-    main: 'app.js',
-    scripts: {
-      start: 'node app.js'
-    },
-    dependencies: {
-      "express": "^4.17.1",
-      "dotenv": "^10.0.0"
-    }
-    // Add more fields as needed
-  }, null, 2);
-  fs.writeFileSync(packageJsonPath, packageJsonContent);
+  // Create testproj-api directory
+  const apiDir = path.join(baseDir, `${projectName}-api`);
+  fs.mkdirSync(apiDir);
 
-  // Generate app.js
-  const appJsPath = path.join(baseDir, 'app.js');
   const appJsCode = `
     const express = require('express');
-    const dotenv = require('dotenv');
-    const path = require('path');
-
-    // Load environment variables from .env file
-    dotenv.config();
-
     const app = express();
 
-    // Your middleware and route setup can go here
+    // Your routes and middleware setup can go here
 
     const PORT = process.env.PORT || 3000;
     app.listen(PORT, () => {
         console.log(\`Server is running on port \${PORT}\`);
     });
     `;
-  fs.writeFileSync(appJsPath, appJsCode.trim());
 
-  // Install packages
+  const formattedAppJsCode = prettier.format(appJsCode, { parser: 'babel' });
+
+  fs.writeFileSync(path.join(apiDir, 'app.js'), formattedAppJsCode.trim());
+
+  // Install dependencies in the api directory
   try {
     console.log('Installing packages...');
-    execSync('npm install', { cwd: baseDir, stdio: 'inherit' });
+    execSync('npm init -y && npm install express dotenv', { cwd: apiDir, stdio: 'inherit' });
     console.log('Packages installed successfully.');
   } catch (error) {
     console.error('Error installing packages:', error);
